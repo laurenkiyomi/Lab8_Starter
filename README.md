c325d1829d856667fd53dd3ca985ebd8751a4f62
laurenkiyomi <laurenklee02@gmail.com>
2022-11-19 08:01:17 -0800

    Explore and expose

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Lab 8 - Starter
+## Lauren Lee
+<ol>
+    <li>I would fit my automated tests (1) within a Github action that runs whenever code is pushed so that my tests are automatically and consistently run as I develop my project.</li>
+    <li>No, I wouldn't use an end to end test to check if a function is returning the correct output. E2e testing is more for simulating user actions from start to end so testing a function could be done without e2e testing by using a unit test.</li>
+    <li>No, I would not use a unit test to test the "message" feature of a 
+    messaging application because there are many parts to writing and sending a message, so 
+    this feature is not necessarily one part of my code that should be 
+    encapsulated in a unit. E2e testing would be more fit because we could simulate 
+    a user writing and sending a message.</li>
+    <li>Yes, I would use a unit test to test the "max message length" feature of a 
+    messaging application because this is a good example of an individual part of my code that I can test in an encapsulated unit by simply calling the max message length function that tests 
+    for the length of a message.</li>
+</ol>
+
+Tests:
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,102 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+describe('isPhoneNumber', () => {
+    // isPhoneNumber should return true
+    test('valid number', () => {
+        expect(functions.isPhoneNumber('408-709-0021')).toBe(true);
+    });
+
+    test('valid number', () => {
+        expect(functions.isPhoneNumber('000-000-0000')).toBe(true);
+    });
+
+    // isPhoneNumber should return false
+    test('invalid number', () => {
+        expect(functions.isPhoneNumber('10')).toBe(false);
+    });
+
+    test('invalid number', () => {
+        expect(functions.isPhoneNumber('111-22-4444')).toBe(false);
+    });
+})
+
+describe('isEmail', () => {
+    // isEmailshould return true
+    test('valid email', () => {
+        expect(functions.isEmail('laurenklee02@gmail.com')).toBe(true);
+    });
+
+    test('valid email', () => {
+        expect(functions.isEmail('laurenklee02@yahoo.com')).toBe(true);
+    });
+
+    // isEmail should return false
+    test('invalid email', () => {
+        expect(functions.isEmail('lauren')).toBe(false);
+    });
+
+    test('invalid email', () => {
+        expect(functions.isEmail('@gmail.com')).toBe(false);
+    });
+})
+
+describe('isStrongPassword', () => {
+    // isStrongPassword should return true
+    test('valid password', () => {
+        expect(functions.isStrongPassword('abcd')).toBe(true);
+    });
+
+    test('valid password', () => {
+        expect(functions.isStrongPassword('abcd_1234_a1b2c')).toBe(true);
+    });
+
+    // isStrongPassword should return false
+    test('invalid password', () => {
+        expect(functions.isStrongPassword('abc')).toBe(false);
+    });
+
+    test('invalid password', () => {
+        expect(functions.isStrongPassword('abcd_1234_a1b2c3')).toBe(false);
+    });
+})
+
+describe('isDate', () => {
+    // isDate should return true
+    test('valid date', () => {
+        expect(functions.isDate('11/19/2022')).toBe(true);
+    });
+
+    test('valid date', () => {
+        expect(functions.isDate('1/1/2023')).toBe(true);
+    });
+
+    // isDate should return false
+    test('invalid date', () => {
+        expect(functions.isDate('1/2022')).toBe(false);
+    });
+
+    test('invalid date', () => {
+        expect(functions.isDate('11/19/22')).toBe(false);
+    });
+})
+
+describe('isHexColor', () => {
+    // isHexColor should return true
+    test('valid hex color', () => {
+        expect(functions.isHexColor('FFF')).toBe(true);
+    });
+
+    test('valid hexcolor', () => {
+        expect(functions.isHexColor('EADDCA')).toBe(true);
+    });
+
+    // isHexColor should return false
+    test('invalid hex color', () => {
+        expect(functions.isHexColor('F')).toBe(false);
+    });
+
+    test('invalid hex color', () => {
+        expect(functions.isHexColor('00')).toBe(false);
+    });
+})
